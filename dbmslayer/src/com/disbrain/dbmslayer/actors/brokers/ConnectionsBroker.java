7e0d7777b6c98c3639bf21eabaf7f265ad0996e6
pfalda <p.falda@disbrain.com>
2014-06-05 11:38:42 +0200

    Closing connections seems that's broken, working on identifying the problem

--- dbmslayer/src/com/disbrain/dbmslayer/actors/brokers/ConnectionsBroker.java
+++ dbmslayer/src/com/disbrain/dbmslayer/actors/brokers/ConnectionsBroker.java
@@ -15,12 +15,60 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ConnectionsBroker extends UntypedActor {
 
+    private static class ConnectionStats {
+
+        private volatile AtomicInteger connections_taken = new AtomicInteger(0);
+        private volatile AtomicInteger errors_taking = new AtomicInteger(0);
+        private volatile AtomicInteger errors_releasing = new AtomicInteger(0);
+        private volatile AtomicInteger totally_used = new AtomicInteger(0);
+
+        public void gotConnection() {
+            totally_used.incrementAndGet();
+            connections_taken.incrementAndGet();
+        }
+
+        public void gotError() {
+            errors_taking.incrementAndGet();
+        }
+
+        public void releaseConnection() {
+            connections_taken.decrementAndGet();
+        }
+
+        public void releaseError() {
+            errors_releasing.incrementAndGet();
+        }
+
+        public long getTaken() {
+            return connections_taken.get();
+        }
+
+        public long getTotalUsage() {
+            return totally_used.get();
+        }
+
+        public long getTakeErrors() {
+            return errors_taking.get();
+        }
+
+        public long getReleaseErrors() {
+            return errors_releasing.get();
+        }
+
+    }
+
+    ;
+
     private final DbmsConnectionPool connection_pool;
-    private static volatile AtomicInteger connections_taken = new AtomicInteger(0);
+    private final static ConnectionStats stats = new ConnectionStats();
     private final LoggingAdapter log;
 
-    public static int getBrokerStats() {
-        return connections_taken.get();
+    public static String getBrokerStats() {
+        return String.format("Currently taken: %d Get errors: %d Release errors: %d Total usage: %d\n",
+                stats.getTaken(),
+                stats.getTakeErrors(),
+                stats.getReleaseErrors(),
+                stats.getTotalUsage());
     }
 
     public ConnectionsBroker(DbmsConnectionPool pool) {
@@ -35,19 +83,23 @@ public class ConnectionsBroker extends UntypedActor {
 
     @Override
     public void onReceive(Object message) {
+
         do {
             if (message instanceof GetDbmsConnectionRequest) {
 
                 Object output;
 
                 try {
+                    Connection connection = connection_pool.getConnection();
+                    if (connection == null) //Timeout while getting a connection
+                        throw new SQLException("Timeout while trying to fetch a connection!");
                     output = new GetDbmsConnectionReply(connection_pool.getConnection(), ((GetDbmsConnectionRequest) message).connection_params);
-                    connections_taken.incrementAndGet();
+                    stats.gotConnection();
                 } catch (SQLException exc) {
-
-                    output = new DbmsConnectionPoolError(exc, connection_pool.getStatistics());
+                    stats.gotError();
+                    output = new DbmsConnectionPoolError(exc, getBrokerStats());
                 }
-
+                log.debug("Get stats: " + getBrokerStats());
                 getSender().tell(output, getSelf());
 
                 break;
@@ -59,28 +111,33 @@ public class ConnectionsBroker extends UntypedActor {
 
                 try {
                     if (close_this.isClosed()) {
+                        stats.gotError();
                         log.error("CANNOT CLOSE OR ROLLBACK A CLOSED CONNECTION!");
                         return;
                     }
                 } catch (SQLException exc) {
+                    stats.gotError();
                     log.error(exc, "ERROR CHECKING CONNECTION STATUS, TRYING TO KEEP GOING");
                 }
                 try {
                     if (close_this.getAutoCommit() == false)
                         close_this.rollback();
                 } catch (SQLException exc) {
+                    stats.gotError();
                     log.error(exc, "ERROR ROLLBACKING A CONNECTION, TRYING TO KEEP GOING");
                 }
                 try {
                     close_this.close();
-                } catch (SQLException exc) {
+                    stats.releaseConnection();
+                } catch (Exception exc) {
+                    stats.gotError();
                     log.error(exc, "CRITICAL ERROR CLOSING A CONNECTION!");
                 }
-                connections_taken.decrementAndGet();
+                log.debug("Close status: " + getBrokerStats());
                 break;
             }
-
-            getSender().tell(new DbmsConnectionPoolError("Unknown message: " + message.getClass()), getSelf());
+            log.error("UNKNOWN: " + message.getClass().getName());
+            getSender().tell(new DbmsConnectionPoolError("Unknown message: " + message.getClass().getName()), getSelf());
 
         } while (false);
 
